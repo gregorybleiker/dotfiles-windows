01e94ae506c7c78a8eb6d85d4404ae70e5f5f738
Gregory Bleiker <gregory.bleiker@bbv.ch>
2020-05-07 11:48:19 +0200

    merged profiles

---
+++ windows-terminal/INSTALL.ps1
@@ -0,0 +1,2 @@
+﻿rm "$env:localappdata\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+cmd /c mklink "$env:localappdata\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" "$env:userprofile\.dotfiles\windows-terminal\settings.json"
